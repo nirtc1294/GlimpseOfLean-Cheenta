c594814c93938f891e5a50bfda1e2d99a41a74dd
shreeshnnayak-bit <shreesh.n.nayak@gmail.com>
2026-07-21 18:47:12 +0530

    again pushed

--- Cheenta_Proofs/Dimension n.lean
+++ Cheenta_Proofs/Dimension n.lean
@@ -8,8 +8,8 @@ import Mathlib.Topology.Sets.OpenCover
 variable {X : Type}[TopologicalSpace X]
 
 def DimLe (S : Set X) : ℕ → Prop
-  | 0 => ∀ {ι : Type} (C : ι → Set X), IsOpenCover C →
-      ∃ (κ : Type) (D : κ → Set X), IsOpenCover D ∧ Refines D C ∧ HasOrderLE D 0
+  | 0 => ∀ {ι : Type} (C : ι → Set X), TopologicalSpace.IsOpenCover C →
+      ∃ (κ : Type) (D : κ → Set X), TopologicalSpace.IsOpenCover D ∧ Refines D C ∧ HasOrderLE D 0
   | n + 1 => ∃ A B : Set X, S = A ∪ B ∧ DimLe A 0 ∧ DimLe B n
 
 lemma dimLe_succ_split {S : Set X} {n : ℕ} :
